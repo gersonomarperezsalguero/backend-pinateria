685f478b2df6088bc07a957409411e5e4f0a7879
gersonomarperezsalguero <gperezs4@miumg.edu.gt>
2025-09-04 17:21:38 -0600

    Update index.js

--- index.js
+++ index.js
@@ -13,7 +13,9 @@ const db = admin.firestore();
 
 const app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: "10mb" }));
+app.use(express.urlencoded({ extended: true, limit: "10mb" }));
+
 
 app.get('/', (req, res) => {
   res.send('API de Piñatería Las Palmas funcionando');
@@ -196,3 +198,4 @@ app.patch('/productos/:id', async (req, res) => {
 });
 
 // 
+
